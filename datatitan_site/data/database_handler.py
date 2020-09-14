26e3321a486917aa18a20209735157af164c6ff9
Emmet Stanevich <emmet@stanevich.com>
2020-09-14 18:19:25 -0400

    Refactored relative paths into file-relative paths
    This will ensure that, regardless of where the project is run from, we'll know EXACTLY where the needed files are. The directories listed here don't currently exist within the repo, so I will need to work out how to handle that.

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -1,16 +1,14 @@
-import csv
 from pathlib import Path
-import numpy as np
 import pandas as pd
 import sqlite3
 
 
-input_file_path = Path("owid-covid-data.csv")
+input_file_path = Path(f"{Path(__file__).parent}/input/owid-covid-data.csv")
+database_path = Path(f"{Path(__file__).parent}/database/test_database.db")
 
-with sqlite3.connect(":memory:") as conn:
+with sqlite3.connect(database_path) as conn:
     # conn.execute('''CREATE TABLE covid_data
     # (iso_code text not null , continent text, location text, date date,
     # new_cases integer default 0, new_deaths integer default 0, new_tests integer default 0)''')
     read_covid_data_raw = pd.read_csv(input_file_path)
     read_covid_data_raw.to_sql("COVID_DATA_RAW", conn, if_exists='replace', index=False)
-
