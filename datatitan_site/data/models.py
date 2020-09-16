2215265ba19e12f44baa3964542ebcb0cc9076ad
NuMellow <cmmukabe@gmail.com>
2020-09-16 16:00:09 -0400

    add blog model and sample html

--- datatitan_site/data/models.py
+++ datatitan_site/data/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class country(models.Model):
@@ -7,3 +8,17 @@ class country(models.Model):
     name = models.CharField(max_length=55)
     continent = models.CharField(max_length=15)
     population = models.IntegerField()
+
+class Post(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def __str__(self):
+    return self.title    
