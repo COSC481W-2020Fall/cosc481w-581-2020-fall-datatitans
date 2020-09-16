887becc1fb0c0127d1e02fc695be0bf1f88d40a2
Emmet Stanevich <emmet@stanevich.com>
2020-09-16 11:39:57 -0400

    Reformatted urls and views files

--- datatitan_site/data/views.py
+++ datatitan_site/data/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'data.html', {})
 
+
 def about(request):
     return render(request, 'about.html', {})
+
 
 def blog(request):
     return render(request, 'blog.html', {})
