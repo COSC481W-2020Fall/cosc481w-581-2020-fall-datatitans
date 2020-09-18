647eee44eaef78ad981662d3815ea5e836c6c0c5
Emmet Stanevich <emmet@stanevich.com>
2020-09-18 16:57:40 -0400

    Added script to generate clean view of covid data
    This view will clean up the data, filling any null values with 0's, and recalculating the running totals per country. Additionally, I have redirected the script to target the db.sqlite database.

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -1,14 +1,33 @@
 from pathlib import Path
 import pandas as pd
 import sqlite3
+from datatitan_site.settings import DATABASES
 
 
 input_file_path = Path(f"{Path(__file__).parent}/input/owid-covid-data.csv")
-database_path = Path(f"{Path(__file__).parent}/database/test_database.db")
+# database_path = Path(f"{Path(__file__).parent}/database/test_database.db")
 
-with sqlite3.connect(database_path) as conn:
-    # conn.execute('''CREATE TABLE covid_data
-    # (iso_code text not null , continent text, location text, date date,
-    # new_cases integer default 0, new_deaths integer default 0, new_tests integer default 0)''')
+with sqlite3.connect(DATABASES["default"]["NAME"]) as conn:
     read_covid_data_raw = pd.read_csv(input_file_path)
-    read_covid_data_raw.to_sql("COVID_DATA_RAW", conn, if_exists='replace', index=False)
+    read_covid_data_raw.to_sql("COVID_DATA_RAW", conn, if_exists="replace", index=False)
+    conn.executescript(
+        """
+        drop view if exists COVID_DATA_CLEAN;
+        CREATE VIEW COVID_DATA_CLEAN as
+        select iso_code,
+               continent,
+               location,
+               date,
+               coalesce(cast(new_cases as integer), 0) as new_cases,
+               sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row)
+               as total_cases,
+               coalesce(cast(new_deaths as integer), 0) as new_deaths,
+               sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
+               as total_deaths,
+               coalesce(cast(new_tests as integer), 0) as new_tests,
+               sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
+               as total_tests
+        from COVID_DATA_RAW
+        order by iso_code, date;
+        """
+    )
