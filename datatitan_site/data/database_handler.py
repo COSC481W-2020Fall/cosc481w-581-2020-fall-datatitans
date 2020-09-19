7f0ed4d37b46efc374df38727daeaea51a850e65
Emmet Stanevich <emmet@stanevich.com>
2020-09-18 23:44:27 -0400

    Placed initialization within methods
    This will allow the initialization to be triggered by the urls.py file.

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -8,39 +8,34 @@ from datetime import date
 input_file_path = Path(f"{Path(__file__).parent}/input/owid-covid-data.csv")
 # database_path = Path(f"{Path(__file__).parent}/database/test_database.db")
 
-if (
-        not input_file_path.exists()
-        or date.fromtimestamp(input_file_path.stat().st_ctime) < date.today()
-):
-    print(f"File Exists: {input_file_path.exists()}")
-    if input_file_path.exists():
-        print(
-            f"File created on:{date.fromtimestamp(input_file_path.stat().st_ctime).today()}\n"
-            f"Current Date: {date.today()}"
+
+def input_missing_or_outdated():
+    return (not input_file_path.exists()) or date.fromtimestamp(input_file_path.stat().st_ctime) < date.today()
+
+
+def initialize_table():
+    with sqlite3.connect(DATABASES["default"]["NAME"]) as conn:
+        read_covid_data_raw = pd.read_csv(input_file_path)
+        read_covid_data_raw.to_sql("COVID_DATA_RAW", conn, if_exists="replace", index=False)
+        conn.executescript(
+            """
+            DROP VIEW IF EXISTS COVID_DATA_CLEAN;
+            CREATE VIEW COVID_DATA_CLEAN as
+            select iso_code,
+                   continent,
+                   location,
+                   date,
+                   cast(coalesce(new_cases, 0) as integer) as new_cases,
+                   cast(sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row) as integer)
+                   as total_cases,
+                   coalesce(cast(new_deaths as integer), 0) as new_deaths,
+                   sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
+                   as total_deaths,
+                   coalesce(cast(new_tests as integer), 0) as new_tests,
+                   sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
+                   as total_tests
+            from COVID_DATA_RAW
+            where iso_code is not null
+            order by iso_code, date;
+            """
         )
-
-
-with sqlite3.connect(DATABASES["default"]["NAME"]) as conn:
-    read_covid_data_raw = pd.read_csv(input_file_path)
-    read_covid_data_raw.to_sql("COVID_DATA_RAW", conn, if_exists="replace", index=False)
-    conn.executescript(
-        """
-        drop view if exists COVID_DATA_CLEAN;
-        CREATE VIEW COVID_DATA_CLEAN as
-        select iso_code,
-               continent,
-               location,
-               date,
-               coalesce(cast(new_cases as integer), 0) as new_cases,
-               sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row)
-               as total_cases,
-               coalesce(cast(new_deaths as integer), 0) as new_deaths,
-               sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
-               as total_deaths,
-               coalesce(cast(new_tests as integer), 0) as new_tests,
-               sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
-               as total_tests
-        from COVID_DATA_RAW
-        order by iso_code, date;
-        """
-    )
