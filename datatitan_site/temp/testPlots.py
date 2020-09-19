ceecac2f0667c418c5006d8bce423a8508b6fe7e
Emmet Stanevich <emmet@stanevich.com>
2020-09-19 17:40:53 -0400

    Reformatted code
    I ran "black" to quickly reformat the code.

--- datatitan_site/temp/testPlots.py
+++ datatitan_site/temp/testPlots.py
@@ -13,13 +13,13 @@ SMALL_SIZE = 8
 MEDIUM_SIZE = 15
 BIGGER_SIZE = 20
 
-plt.rc('font', size=SMALL_SIZE)  # default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc("font", size=SMALL_SIZE)  # default text sizes
+plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 DataDir = Path(__file__).parent.parent / "data/input"
 ImageDir = Path(__file__).parent.parent / "images"
@@ -32,7 +32,7 @@ df.head()
 # df.describe()                          # list out data frame
 plt.figure(figsize=(50, 20))  # set size of the graph
 sns.set(font_scale=3)  # set scale of graph fonts
-sns.lineplot(data=df['total_deaths'])  # set line plot from data
+sns.lineplot(data=df["total_deaths"])  # set line plot from data
 # plt.ylim(0,250)                        # function to adjust the y limit
 # plt.xlim(0,250)                        # function to adjust the x limit
 plt.xticks(rotation=-45)  # add an angle to x labels
@@ -81,7 +81,9 @@ df.describe()  # list data
 dims = (50, 100)  # set new dimension variable for plot area
 fig, ax = plt.subplots(figsize=dims)  # set plot area size
 sns.set(font_scale=1)  # scale down font size
-sns.barplot(x=df["total_deaths"], y=df["location"])  # big bar graph of full original data frame
+sns.barplot(
+    x=df["total_deaths"], y=df["location"]
+)  # big bar graph of full original data frame
 plt.autoscale()
 plt.xticks(rotation=-45)
 plt.savefig(ImageDir / "plot3.jpeg")
