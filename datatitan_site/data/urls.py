27f2420fccfa30f242a7c10f89d1263a6992c137
Emmet Stanevich <emmet@stanevich.com>
2020-09-18 23:46:05 -0400

    Added database initialization to urls.py

--- datatitan_site/data/urls.py
+++ datatitan_site/data/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views
+from . import views, database_handler
+import urllib.request
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('blog/', views.blog, name='blog'),
 ]
+
+if database_handler.input_missing_or_outdated():
+    urllib.request.urlretrieve(url="https://covid.ourworldindata.org/data/owid-covid-data.csv",
+                               filename=database_handler.input_file_path)
+database_handler.initialize_table()
