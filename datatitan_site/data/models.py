2093977e477f142e55a126081a5215f55048f63d
Emmet Stanevich <emmet@stanevich.com>
2020-09-19 13:03:13 -0400

    Cleaned up models.py
    I simply had the IDE reformat the file for this change.

--- datatitan_site/data/models.py
+++ datatitan_site/data/models.py
@@ -24,16 +24,19 @@ class CovidDataClean(models.Model):
     total_tests = models.IntegerField()
     # population = models.PositiveIntegerField(default=0)
 
+
 class Post(models.Model):
     author = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default = timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
 
 def publish(self):
     self.published_date = timezone.now()
     self.save()
 
+
 def __str__(self):
-    return self.title    
+    return self.title
