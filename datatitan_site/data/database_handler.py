586efe0addc9bc916ce088ba706b76e03c13f7ab
Emmet Stanevich <emmet@stanevich.com>
2020-09-20 19:35:20 -0400

    Added code to regenerate the cleaned data table

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlite3
 from datatitan_site.settings import DATABASES
 from datetime import date
-from .models import CovidDataRaw
+from .models import CovidDataRaw, CovidDataClean
 
 
 input_file_path = Path(f"{Path(__file__).parent}/input/owid-covid-data.csv")
@@ -46,3 +46,24 @@ def initialize_table():
             order by iso_code, date;
             """
         )
+        pd.read_sql_query(
+            """
+            select iso_code,
+                   continent,
+                   location,
+                   date,
+                   coalesce(cast(new_cases as integer), 0) as new_cases,
+                   cast(sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row) as integer)
+                   as total_cases,
+                   coalesce(cast(new_deaths as integer), 0) as new_deaths,
+                   sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
+                   as total_deaths,
+                   coalesce(cast(new_tests as integer), 0) as new_tests,
+                   sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
+                   as total_tests
+            from data_coviddataraw
+            where iso_code is not null
+            order by iso_code, date;
+            """,
+            con=conn,
+        ).to_sql(CovidDataClean._meta.db_table, con=conn, index=False, if_exists="append")
