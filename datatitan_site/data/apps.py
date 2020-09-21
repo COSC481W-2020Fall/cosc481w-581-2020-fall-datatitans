f3d640f4b5b2c3841b4f2392239afbb05cd444ae
Emmet Stanevich <emmet@stanevich.com>
2020-09-21 19:25:44 -0400

    Moved initialization code to apps.py
    This should help avoid errors in the startup, but won't necessarily be good enough for a production environment.

--- datatitan_site/data/apps.py
+++ datatitan_site/data/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+import sys
+import urllib.request
 
 
 class DataConfig(AppConfig):
     name = 'data'
+
+    def ready(self):
+        if 'runserver' not in sys.argv:
+            return True
+        from .database_handler import input_missing_or_outdated, input_file_path, initialize_table
+        from .models import CovidDataRaw
+        if input_missing_or_outdated():
+            urllib.request.urlretrieve(url="https://covid.ourworldindata.org/data/owid-covid-data.csv",
+                                       filename=input_file_path)
+            initialize_table()
+        elif not CovidDataRaw.objects.exists():
+            initialize_table()
--- datatitan_site/data/urls.py
+++ datatitan_site/data/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from . import views, database_handler
-import urllib.request
-from .models import CovidDataRaw
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,10 +7,3 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('blog_detail/<blog_id>', views.blog_detail, name='blog_detail'),
 ]
-
-if database_handler.input_missing_or_outdated():
-    urllib.request.urlretrieve(url="https://covid.ourworldindata.org/data/owid-covid-data.csv",
-                               filename=database_handler.input_file_path)
-    database_handler.initialize_table()
-elif not CovidDataRaw.objects.exists():
-    database_handler.initialize_table()
