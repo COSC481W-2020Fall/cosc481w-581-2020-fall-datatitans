b2778b3bd8823468a26a3d62a2036e65ac33ecdd
Irongaru <bpotte11@emich.edu>
2020-09-21 14:20:12 -0400

    almost done with jpeg generation

--- datatitan_site/temp/PrototypeGraphSetup.py
+++ datatitan_site/temp/PrototypeGraphSetup.py
@@ -12,6 +12,9 @@ import pandas as pd # Because pandas are cute and cuddly
 import seaborn as sns # For plotting
 import matplotlib.pyplot as plt # For showing plots
 import mpld3 #for generating html
+from pathlib import Path
+ImageDir = Path(__file__).parent.parent / "images"
+
 cN = ["USA","CAN","MEX"]
 for Q in cN:
     data = dictNA[Q]['data']
@@ -23,7 +26,19 @@ for Q in cN:
     fig1 = plt.gcf()
     plt.show()
     plt.draw()
-    fig1.savefig(Q+'1.jpeg', dpi=100)
+    fig1.savefig(ImageDir / Q+'1.jpeg', dpi=1000)
+
+for W in cN:
+    data = dictNA[W]['data']
+    df = pd.DataFrame(data)
+    a = sns.lineplot(data=df, x = "date", y ="deaths")
+    plt.title("deaths vs date in "+Q)
+    plt.xlabel('Dates')
+    plt.ylabel('Deaths')
+    fig1 = plt.gcf()
+    plt.show()
+    plt.draw()
+    fig1.savefig(ImageDir / Q+'2.jpeg', dpi=1000)
 
 # This next line doesn't work. We have to cast the "date" field as date and figure out how to
 # show the dates on the x-axis
