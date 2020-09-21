59ed8dcb42438b800730afbfbe7f3a9e2f5ad73a
Emmet Stanevich <emmet@stanevich.com>
2020-09-21 19:10:00 -0400

    Removed view from database
    It turns out views probably aren't what we're looking for.

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -24,28 +24,6 @@ def initialize_table():
             CovidDataRaw._meta.db_table, conn, index=False, if_exists="append"
         )
         CovidDataClean.objects.all().delete()
-        conn.executescript(
-            """
-            DROP VIEW IF EXISTS COVID_DATA_CLEAN;
-            CREATE VIEW COVID_DATA_CLEAN as
-            select iso_code,
-                   continent,
-                   location,
-                   date,
-                   coalesce(new_cases, 0) as new_cases,
-                   sum(coalesce(new_cases, 0)) over (partition by iso_code rows between unbounded preceding and current row)
-                   as total_cases,
-                   coalesce(new_deaths, 0) as new_deaths,
-                   sum(coalesce(new_deaths, 0)) over (partition by iso_code rows between unbounded preceding and current row)
-                   as total_deaths,
-                   coalesce(new_tests, 0) as new_tests,
-                   sum(coalesce(new_tests, 0)) over (partition by iso_code rows between unbounded preceding and current row)
-                   as total_tests
-            from data_coviddataraw
-            where iso_code is not null
-            order by iso_code, date;
-            """
-        )
         pd.read_sql_query(
             """
             select iso_code,
