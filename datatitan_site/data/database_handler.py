51c082032fb44353e26afc451fefe933eee19f47
Emmet Stanevich <emmet@stanevich.com>
2020-09-22 17:26:10 -0400

    Updated SQL query to reflect updated model
    This is where all the logic is, filling in null values and recalculating aggregate columns. I also took the liberty of re-indenting the SQL code.

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -30,30 +30,62 @@ def initialize_table():
                    continent,
                    location,
                    date,
+                   total_cases,
                    new_cases,
-                   sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row)
-                   as total_cases,
-                   avg(new_cases) over (partition by iso_code rows between 6 preceding and current row)
-                   as new_cases_smoothed,
+                   new_cases_smoothed,
+                   total_deaths,
                    new_deaths,
-                   sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
-                   as total_deaths,
-                   avg(new_deaths) over (partition by iso_code rows between 6 preceding and current row)
-                   as new_deaths_smoothed,
+                   new_deaths_smoothed,
+                   total_cases / (population / 1000000) as total_cases_per_million,
+                   new_cases / (population / 1000000) as new_cases_per_million,
                    new_tests,
-                   sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
-                   as total_tests,
-                   avg(new_tests) over (partition by iso_code rows between 6 preceding and current row)
-                   as new_tests_smoothed
+                   total_tests,
+                   total_tests / (population / 1000) as total_tests_per_thousand,
+                   new_tests / (population / 1000) as new_tests_per_thousand,
+                   new_tests_smoothed,
+                   new_tests_smoothed / (population / 1000) as new_tests_smoothed_per_thousand,
+                   avg(new_tests / new_cases) over (partition by iso_code rows between 6 preceding and current row)
+                       as tests_per_case,
+                   avg(new_cases / new_tests) over (partition by iso_code rows between 6 preceding and current row)
+                       as positive_rate,
+                   tests_units,
+                   stringency_index,
+                   population
             from (
-            select iso_code,
-            continent,
-            location,
-            date,
-            coalesce(new_cases, 0) as new_cases,
-            coalesce(new_deaths, 0) as new_deaths,
-            coalesce(new_tests, 0) as new_tests
-            from data_coviddataraw)
+                select iso_code,
+                       continent,
+                       location,
+                       date,
+                       sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row)
+                           as total_cases,
+                       new_cases,
+                       avg(new_cases) over (partition by iso_code rows between 6 preceding and current row)
+                           as new_cases_smoothed,
+                       sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
+                           as total_deaths,
+                       new_deaths,
+                       avg(new_deaths) over (partition by iso_code rows between 6 preceding and current row)
+                           as new_deaths_smoothed,
+                       new_tests,
+                       sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
+                           as total_tests,
+                       avg(new_tests) over (partition by iso_code rows between 6 preceding and current row)
+                           as new_tests_smoothed,
+                       tests_units,
+                       stringency_index,
+                       population
+                from (
+                    select iso_code,
+                           continent,
+                           location,
+                           date,
+                           coalesce(new_cases, 0) as new_cases,
+                           coalesce(new_deaths, 0) as new_deaths,
+                           coalesce(new_tests, 0) as new_tests,
+                           tests_units,
+                           coalesce(stringency_index, 0.0) as stringency_index,
+                           population
+                    from data_coviddataraw))
             where iso_code is not null and continent is not null
             order by iso_code, date;
             """,
