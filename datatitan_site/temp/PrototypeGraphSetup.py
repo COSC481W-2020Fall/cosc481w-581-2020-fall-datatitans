bb208a1bc9bf71bd8d23627d9d1545f88db28aec
Jeff Pollack <JeffPollack@users.noreply.github.com>
2020-09-22 19:15:55 -0400

    adjusting the plot images

--- datatitan_site/temp/PrototypeGraphSetup.py
+++ datatitan_site/temp/PrototypeGraphSetup.py
@@ -10,7 +10,18 @@ from pathlib import Path
 
 #print(dictNA['USA']['data'])
 
+SMALL_SIZE = 5
+SMALLER_SIZE = 3
+MEDIUM_SIZE = 15
+BIGGER_SIZE = 20
 
+plt.rc("font", size=SMALL_SIZE)  # default text sizes
+plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALLER_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=BIGGER_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
 
@@ -24,26 +35,42 @@ def gen_images():
         data = dictNA[Q]['data']
         df = pd.DataFrame(data)
         a = sns.lineplot(data=df, x = "date", y ="total_cases")
+        for ind, label in enumerate(
+                a.get_xticklabels()
+        ):  # loop so not every xtick is show,
+            if ind % 10 == 0:  # making chart more readable
+                label.set_visible(True)
+            else:
+                label.set_visible(False)
         plt.title("total cases in "+Q)
         plt.xlabel('Dates')
         plt.ylabel('Total Deaths')
+        plt.xticks(rotation=-45)  # add an angle to x labels
         fig1 = plt.gcf()
         plt.draw()
         fileN = Q+'1.jpeg'
-        fig1.savefig(ImageDir / fileN, dpi=100)
+        fig1.savefig(ImageDir / fileN, dpi=400)
         plt.close(fig1)
 
     for W in cN:
         data = dictNA[W]['data']
         df = pd.DataFrame(data)
         a = sns.lineplot(data=df, x = "date", y ="total_deaths")
+        for ind, label in enumerate(
+                a.get_xticklabels()
+        ):  # loop so not every xtick is show,
+            if ind % 10 == 0:  # making chart more readable
+                label.set_visible(True)
+            else:
+                label.set_visible(False)
         plt.title("total deaths in "+W)
         plt.xlabel('Dates')
         plt.ylabel('Deaths')
+        plt.xticks(rotation=-45)  # add an angle to x labels
         fig1 = plt.gcf()
         plt.draw()
         fileN = W+'2.jpeg'
-        fig1.savefig(ImageDir / fileN, dpi=100)
+        fig1.savefig(ImageDir / fileN, dpi=400)
         plt.close(fig1)
 
     # This next line doesn't work. We have to cast the "date" field as date and figure out how to
