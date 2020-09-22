e0878380fec82cc7fc1f9b89fdd5ca82e187cfa9
Emmet Stanevich <emmet@stanevich.com>
2020-09-22 10:51:05 -0400

    Refactored prototype graph setup
    The variables need to be given more informative names

--- datatitan_site/temp/PrototypeGraphSetup.py
+++ datatitan_site/temp/PrototypeGraphSetup.py
@@ -13,34 +13,33 @@ from pathlib import Path
 
 def gen_images():
     with Path(__file__).parent / "dataNA.json" as f:
-        dictNA = json.load(f.open('r'))
+        dictNA = json.load(f.open("r"))
     ImageDir = Path(__file__).parent.parent / "data" / "static"
 
     cN = ["USA", "CAN", "MEX"]
+    # TODO: Give variables more informative names
     for Q in cN:
-        data = dictNA[Q]['data']
+        data = dictNA[Q]["data"]
         df = pd.DataFrame(data)
-        a = sns.lineplot(data=df, x = "date", y ="total_cases")
-        plt.title("total cases in "+Q)
-        plt.xlabel('Dates')
-        plt.ylabel('Total Deaths')
+        a = sns.lineplot(data=df, x="date", y="total_cases")
+        plt.title("total cases in " + Q)
+        plt.xlabel("Dates")
+        plt.ylabel("Total Deaths")
         fig1 = plt.gcf()
         plt.draw()
-        fileN = Q+'1.jpeg'
-        fig1.savefig(ImageDir / fileN, dpi=100)
+        fig1.savefig(ImageDir / (Q + "1.jpeg"), dpi=100)
         plt.close(fig1)
 
     for W in cN:
-        data = dictNA[W]['data']
+        data = dictNA[W]["data"]
         df = pd.DataFrame(data)
-        a = sns.lineplot(data=df, x = "date", y ="total_deaths")
-        plt.title("total deaths in "+W)
-        plt.xlabel('Dates')
-        plt.ylabel('Deaths')
+        a = sns.lineplot(data=df, x="date", y="total_deaths")
+        plt.title("total deaths in " + W)
+        plt.xlabel("Dates")
+        plt.ylabel("Deaths")
         fig1 = plt.gcf()
         plt.draw()
-        fileN = W+'2.jpeg'
-        fig1.savefig(ImageDir / fileN, dpi=100)
+        fig1.savefig(ImageDir / (W + "2.jpeg"), dpi=100)
         plt.close(fig1)
 
     # This next line doesn't work. We have to cast the "date" field as date and figure out how to
