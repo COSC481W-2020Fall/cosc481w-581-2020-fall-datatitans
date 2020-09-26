799ae01dffe2de5e9b4be3d4993b6cbb2d2baaaa
Emmet Stanevich <emmet@stanevich.com>
2020-09-26 15:18:42 -0400

    Made data page require a "GET" request
    By making sure that the data page requires a "GET" request, we don't need to add any conditional handlers for other incoming request types; we just add the decorator, and any "POST" requests directed towards that view will bonk.

--- datatitan_site/data/views.py
+++ datatitan_site/data/views.py
@@ -4,8 +4,10 @@ from .models import Post, Country
 from pathlib import Path
 from .scripts.generate_graphs import gen_graph
 from django.utils.datastructures import MultiValueDictKeyError
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def home(request):
     img_url = "static/USA1.jpeg"
     selected_country = "USA"
@@ -14,44 +16,29 @@ def home(request):
     data_type = [("TOTAL_CASES", "Total Cases"), ("TOTAL_DEATHS", "Total Deaths")]
     category_name = {"total_cases": "Total Cases", "total_deaths": "Total Deaths"}
 
-    if request.method == "GET":
-        # Get items from the form
-        # form = ListForm(request.POST or None)
-        form = request.GET
+    # Get items from the form
+    # form = ListForm(request.POST or None)
+    form = request.GET
 
-        try:
-            country_code = form["country_code"]
-            chart_type = form["data_code"]
-        except MultiValueDictKeyError:
-            country_code = "USA"
-            chart_type = "TOTAL_CASES"
-        return render(
-            request,
-            "data.html",
-            {
-                "chart": gen_graph(
-                    iso_code=country_code, category=str.lower(chart_type)
-                ),
-                "countries": countries,
-                "selected_country": Country.objects.get(country_code=country_code).name,
-                "data_type": data_type,
-                "selected_data": category_name[str.lower(chart_type)],
-            },
-        )
-    else:
-
-        # chart = seaborn(country_code, chart_type)
-        return render(
-            request,
-            "data.html",
-            {
-                "chart": img_url,
-                "countries": countries,
-                "selected_country": selected_country,
-                "data_type": data_type,
-                "selected_data": selected_data,
-            },
-        )
+    try:
+        country_code = form["country_code"]
+        chart_type = form["data_code"]
+    except MultiValueDictKeyError:
+        country_code = "USA"
+        chart_type = "TOTAL_CASES"
+    return render(
+        request,
+        "data.html",
+        {
+            "chart": gen_graph(
+                iso_code=country_code, category=str.lower(chart_type)
+            ),
+            "countries": countries,
+            "selected_country": Country.objects.get(country_code=country_code).name,
+            "data_type": data_type,
+            "selected_data": category_name[str.lower(chart_type)],
+        },
+    )
 
 
 def about(request):
