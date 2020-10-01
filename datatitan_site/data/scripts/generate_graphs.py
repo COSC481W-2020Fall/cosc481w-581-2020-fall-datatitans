72ccb885b59d1f822d5c9c9cda9954887513ba22
Jeff Pollack <JeffPollack@users.noreply.github.com>
2020-10-01 15:29:04 -0400

    adjusting the plot images

--- datatitan_site/data/scripts/generate_graphs.py
+++ datatitan_site/data/scripts/generate_graphs.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import mpld3
 from ..models import CovidDataClean, Country
 
-SMALL_SIZE = 8
+SMALL_SIZE = 10
 SMALLER_SIZE = 3
-MEDIUM_SIZE = 15
-BIGGER_SIZE = 20
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 30
 
 plt.rc("font", size=MEDIUM_SIZE)  # default text sizes
 plt.rc("axes", titlesize=MEDIUM_SIZE)  # fontsize of the axes title
@@ -15,20 +15,29 @@ plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
 plt.rc("legend", fontsize=BIGGER_SIZE)  # legend fontsize
 plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
+dims = (8, 4)  # dimension variable for plot area
+
 
 def gen_graph(iso_code: str, category: str, chart_type="line"):
     category_name = {"total_cases": "Total Cases", "total_deaths": "Total Deaths"}
+    plt.subplots(figsize=dims)
     plt.title(
         category_name[category]
         + " in "
         + Country.objects.get(country_code=iso_code).name
     )
     target_query = CovidDataClean.objects.filter(iso_code__exact=iso_code).order_by("date")
-    plt.plot(target_query.values_list("date"), target_query.values_list(category))
+    plt.plot(target_query.values_list("date"), target_query.values_list(category), label=iso_code)
+    plt.legend(shadow=True, fancybox=True, loc=2, prop={'size': 10})
     plt.xlabel("Dates")
-    plt.ylabel(category_name[category])
+
+    if iso_code == "USA" and category == "total_cases":
+        plt.ylabel(category_name[category], labelpad=36)
+    else:
+        plt.ylabel(category_name[category])
+
     figure = plt.gcf()
-    plt.draw()
+    #plt.draw()
     graph_output = mpld3.fig_to_html(figure)
     plt.close(figure)
     return graph_output
