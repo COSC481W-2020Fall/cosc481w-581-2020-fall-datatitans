fe6de19b5c47e84f66f50fa2beb500f8f04a145c
Emmet Stanevich <emmet@stanevich.com>
2020-10-01 15:14:40 -0400

    Replaced raw SQL statements to clean COVID data with QuerySet
    This change serves a similar purpose as the previous commit, this time for the cleaned-up data.

--- datatitan_site/data/database_handler.py
+++ datatitan_site/data/database_handler.py
@@ -32,80 +32,161 @@ def initialize_table():
         read_covid_data_raw.to_sql(
             CovidDataRaw._meta.db_table, conn, index=False, if_exists="append"
         )
-        CovidDataClean.objects.all().delete()
-        pd.read_sql_query(
-            """
-            select iso_code,
-                   continent,
-                   location,
-                   date,
-                   total_cases,
-                   new_cases,
-                   new_cases_smoothed,
-                   total_deaths,
-                   new_deaths,
-                   new_deaths_smoothed,
-                   total_cases / pop_float * 1000000 as total_cases_per_million,
-                   new_cases / pop_float * 1000000 as new_cases_per_million,
-                   new_cases_smoothed / pop_float * 1000000 as new_cases_smoothed_per_million,
-                   total_deaths / pop_float * 1000000 as total_deaths_per_million,
-                   new_deaths / pop_float * 1000000 as new_deaths_per_million,
-                   new_deaths_smoothed / pop_float * 1000000 as new_deaths_smoothed_per_million,
-                   new_tests,
-                   total_tests,
-                   total_tests / pop_float * 1000 as total_tests_per_thousand,
-                   new_tests / pop_float * 1000 as new_tests_per_thousand,
-                   new_tests_smoothed,
-                   new_tests_smoothed / pop_float * 1000 as new_tests_smoothed_per_thousand,
-                   avg(new_tests / new_cases) over (partition by iso_code rows between 6 preceding and current row)
-                       as tests_per_case,
-                   avg(new_cases / new_tests) over (partition by iso_code rows between 6 preceding and current row)
-                       as positive_rate,
-                   tests_units,
-                   stringency_index,
-                   population
-            from (
-                select iso_code,
-                       continent,
-                       location,
-                       date,
-                       sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row)
-                           as total_cases,
-                       new_cases,
-                       avg(new_cases) over (partition by iso_code rows between 6 preceding and current row)
-                           as new_cases_smoothed,
-                       sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
-                           as total_deaths,
-                       new_deaths,
-                       avg(new_deaths) over (partition by iso_code rows between 6 preceding and current row)
-                           as new_deaths_smoothed,
-                       new_tests,
-                       sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
-                           as total_tests,
-                       avg(new_tests) over (partition by iso_code rows between 6 preceding and current row)
-                           as new_tests_smoothed,
-                       tests_units,
-                       stringency_index,
-                       population,
-                       cast(population as real) as pop_float
-                from (
-                    select iso_code,
-                           continent,
-                           location,
-                           date,
-                           coalesce(new_cases, 0) as new_cases,
-                           coalesce(new_deaths, 0) as new_deaths,
-                           coalesce(new_tests, 0) as new_tests,
-                           tests_units,
-                           coalesce(stringency_index, 0.0) as stringency_index,
-                           population
-                    from data_coviddataraw))
-            where iso_code is not null and continent is not null
-            order by iso_code, date;
-            """,
-            con=conn,
-        ).to_sql(
-            CovidDataClean._meta.db_table, con=conn, index=False, if_exists="append"
+        # CovidDataClean.objects.all().delete()
+        # In[1]:
+        window = {"partition_by": F("iso_code"), "order_by": [F("date")]}
+        past_week = RowRange(start=-6, end=0)
+        clean_data = (
+            CovidDataRaw.objects.exclude(iso_code__isnull=True)
+            .values("iso_code", "continent", "location", "date",)
+            .annotate(
+                total_cases=Window(expression=Sum(F("new_cases")), **window),
+                new_cases=Coalesce(F("new_cases"), 0),
+                new_cases_smoothed=Window(
+                    expression=Avg(F("new_cases")),
+                    frame=RowRange(start=-6, end=0),
+                    **window
+                ),
+                new_deaths=Coalesce(F("new_deaths"), 0),
+                total_deaths=Window(expression=Sum(F("new_deaths")), **window),
+                new_deaths_smoothed=Window(
+                    expression=Avg(F("new_deaths")),
+                    frame=RowRange(start=-6, end=0),
+                    **window
+                ),
+                new_tests=Coalesce(F("new_tests"), 0),
+                total_tests=Window(expression=Sum(F("new_tests")), **window),
+                new_tests_smoothed=Window(
+                    expression=Avg(F("new_tests")),
+                    frame=RowRange(start=-6, end=0),
+                    **window
+                ),
+            )
+            .annotate(
+                total_cases_per_million=Cast(F("total_cases"), FloatField())
+                / Cast(F("population"), FloatField())
+                * 1000000,
+                new_cases_per_million=Cast(F("new_cases"), FloatField())
+                / Cast(F("population"), FloatField())
+                * 1000000,
+                new_cases_smoothed_per_million=F("new_cases_smoothed")
+                / Cast(F("population"), FloatField())
+                * 1000000,
+                total_deaths_per_million=Cast(F("total_deaths"), FloatField())
+                / Cast(F("population"), FloatField())
+                * 1000000,
+                new_deaths_per_million=Cast(F("new_deaths"), FloatField())
+                / Cast(F("population"), FloatField())
+                * 1000000,
+                new_deaths_smoothed_per_million=F("new_deaths_smoothed")
+                / Cast(F("population"), FloatField())
+                * 1000000,
+                total_tests_per_thousand=Cast(F("total_tests"), FloatField())
+                / Cast(F("population"), FloatField())
+                * 1000,
+                new_tests_per_thousand=Cast(F("new_tests"), FloatField())
+                / Cast(F("population"), FloatField())
+                * 1000,
+                new_tests_smoothed_per_thousand=F("new_tests_smoothed")
+                / Cast(F("population"), FloatField())
+                * 1000,
+                tests_per_case=Window(
+                    expression=Avg(
+                        Cast(F("new_tests"), FloatField())
+                        / Cast(F("new_cases"), FloatField())
+                    ),
+                    frame=RowRange(start=-6, end=0),
+                    **window
+                ),
+                positive_rate=Window(
+                    expression=Avg(
+                        Cast(F("new_cases"), FloatField())
+                        / Cast(F("new_tests"), FloatField())
+                    ),
+                    frame=RowRange(start=-6, end=0),
+                    **window
+                ),
+            )
+            .annotate(
+                tests_units=F("tests_units"),
+                stringency_index=F("stringency_index"),
+                population=F("population"),
+            )
+        )
+        # pd.read_sql_query(
+        #     """
+        #     select iso_code,
+        #            continent,
+        #            location,
+        #            date,
+        #            total_cases,
+        #            new_cases,
+        #            new_cases_smoothed,
+        #            total_deaths,
+        #            new_deaths,
+        #            new_deaths_smoothed,
+        #            total_cases / pop_float * 1000000 as total_cases_per_million,
+        #            new_cases / pop_float * 1000000 as new_cases_per_million,
+        #            new_cases_smoothed / pop_float * 1000000 as new_cases_smoothed_per_million,
+        #            total_deaths / pop_float * 1000000 as total_deaths_per_million,
+        #            new_deaths / pop_float * 1000000 as new_deaths_per_million,
+        #            new_deaths_smoothed / pop_float * 1000000 as new_deaths_smoothed_per_million,
+        #            new_tests,
+        #            total_tests,
+        #            total_tests / pop_float * 1000 as total_tests_per_thousand,
+        #            new_tests / pop_float * 1000 as new_tests_per_thousand,
+        #            new_tests_smoothed,
+        #            new_tests_smoothed / pop_float * 1000 as new_tests_smoothed_per_thousand,
+        #            avg(new_tests / new_cases) over (partition by iso_code rows between 6 preceding and current row)
+        #                as tests_per_case,
+        #            avg(new_cases / new_tests) over (partition by iso_code rows between 6 preceding and current row)
+        #                as positive_rate,
+        #            tests_units,
+        #            stringency_index,
+        #            population
+        #     from (
+        #         select iso_code,
+        #                continent,
+        #                location,
+        #                date,
+        #                sum(new_cases) over (partition by iso_code rows between unbounded preceding and current row)
+        #                    as total_cases,
+        #                new_cases,
+        #                avg(new_cases) over (partition by iso_code rows between 6 preceding and current row)
+        #                    as new_cases_smoothed,
+        #                sum(new_deaths) over (partition by iso_code rows between unbounded preceding and current row)
+        #                    as total_deaths,
+        #                new_deaths,
+        #                avg(new_deaths) over (partition by iso_code rows between 6 preceding and current row)
+        #                    as new_deaths_smoothed,
+        #                new_tests,
+        #                sum(new_tests) over (partition by iso_code rows between unbounded preceding and current row)
+        #                    as total_tests,
+        #                avg(new_tests) over (partition by iso_code rows between 6 preceding and current row)
+        #                    as new_tests_smoothed,
+        #                tests_units,
+        #                stringency_index,
+        #                population,
+        #                cast(population as real) as pop_float
+        #         from (
+        #             select iso_code,
+        #                    continent,
+        #                    location,
+        #                    date,
+        #                    coalesce(new_cases, 0) as new_cases,
+        #                    coalesce(new_deaths, 0) as new_deaths,
+        #                    coalesce(new_tests, 0) as new_tests,
+        #                    tests_units,
+        #                    coalesce(stringency_index, 0.0) as stringency_index,
+        #                    population
+        #             from data_coviddataraw))
+        #     where iso_code is not null and continent is not null
+        #     order by iso_code, date;
+        #     """,
+        #     con=conn,
+        # ).to_sql(
+        #     CovidDataClean._meta.db_table, con=conn, index=False, if_exists="append"
+        # )
         CovidDataClean.objects.bulk_create([CovidDataClean(**row) for row in clean_data], ignore_conflicts=True)
         # Country.objects.all().delete()
         # pd.read_sql_query(
