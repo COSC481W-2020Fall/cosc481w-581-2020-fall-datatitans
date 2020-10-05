8d7f7a1879c6a6e6c4ca35ac200d50b29d607563
Emmet Stanevich <emmet@stanevich.com>
2020-10-05 19:57:31 -0400

    Added docstring to generate_graphs.py
    A docstring is much better than a bunch of commented-out lines, as it also provides context for the IDE.

--- datatitan_site/data/scripts/generate_graphs.py
+++ datatitan_site/data/scripts/generate_graphs.py
@@ -1,8 +1,3 @@
-# generate_graphs.py uses the gen_graph function to create an image of a chart based on country codes (iso), category, and chart_type.
-# At this point, chart type is not needed as line chart is the only option
-# It can take a limitless number of country inputs
-# the gen_graph function is called from the views file.
-
 import matplotlib.pyplot as plt
 import mpld3
 from data.models import CovidDataClean, Country
@@ -24,6 +19,13 @@ dims = (8, 4)  # dimension variable for plot area
 
 
 def gen_graph(*iso_codes, category: str, chart_type="line"):
+    """Creates a graph that tracks a data category over time for an arbitrary number of countries.
+
+    :param iso_codes: ISO codes of countries to create graphs for
+    :param category: The category of data to track (currently supported categories: total_cases, total_deaths)
+    :param chart_type: The type of graph to generate (currently supported graphs: line)
+    :return: HTML string representing the generated graph
+    """
     category_name = {"total_cases": "Total Cases", "total_deaths": "Total Deaths"}
     plt.subplots(figsize=dims)
 
