edec5a1b8e9de390c81d2ed070fd78b8e81b39b1
Emmet Stanevich <emmet@stanevich.com>
2020-10-21 15:08:24 -0400

    Added database test case
    This test case is mainly to ensure that my implementation of Django's ORM works for importing the data from the CSV file into a table.

--- datatitan_site/data/tests.py
+++ datatitan_site/data/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from data.models import CovidDataClean, CovidDataRaw
+import pandas as pd
+from data.scripts.database_handler import input_file_path
+
 
 # Create your tests here.
+
+
+class DatabaseTestCase(TestCase):
+    def setUp(self) -> None:
+        self.raw_data = pd.read_csv(
+            input_file_path,
+            # dtype={
+            #     "iso_code": "string",
+            #     "continent": "string",
+            #     "tests_units": "string",
+            # },
+        )
+        decimals = {
+            "stringency_index": 2,
+            "median_age": 1,
+            "extreme_poverty": 1,
+            "diabetes_prevalence": 2,
+            "life_expectancy": 2
+        }
+        self.raw_data = self.raw_data.round(decimals=3).round(decimals=decimals).where(self.raw_data.notnull(), None)
+
+    def test_upload(self):
+        CovidDataRaw.objects.bulk_create(
+            [CovidDataRaw(**row) for row in self.raw_data.to_dict("records")], ignore_conflicts=True, batch_size=1000
+        )
+        print(CovidDataRaw.objects.count())
+        if CovidDataRaw.objects.count() != len(self.raw_data):
+            raise AssertionError(f"Expected number of rows: {len(self.raw_data)}; Actual number of rows: {CovidDataRaw.objects.count()}")
