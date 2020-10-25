5ca6154d4ad7b743985c8d8583fa3dc3eb493fae
rhayes13 <rhayes13@emich.edu>
2020-10-25 03:08:09 -0400

    update chart unit test

--- datatitan_site/data/test_chart_type.py
+++ datatitan_site/data/test_chart_type.py
@@ -1,28 +1,45 @@
 import unittest
 
 
-
 class ChartTest(unittest.TestCase):
     
     # Naming convention: start names of all test functions with the word "test"
     def setUp(self):
         # Arrange
-        self.codes = ("USA", "CAN", "MEX")
         self.nocodes = ()
-        self.str1 = ("astring")
+        self.none = ()
+        self.line = ("LINE")
         
     def tearDown(self):
         # print("tearDown called...")
         self.a = ()
         self.str1 = ()
     
-    def test_graph(self):
-        # Act
-        result = gen_graph(*self.nocodes, category=str(self.str1))
+    def test_no_chart(self):
+        result = gen_no_graph(*self.nocodes, category=str(""))
         
         # Assert
-        self.assertEqual(result, "")
+        self.assertEqual(result, None)
 
+    def test_line_chart(self):
+        # No function to call in forms.py, other than init
+        result = gen_line_graph(*self.nocodes, category=str("LINE"))
+        result = "LINE"
+        # Assert
+        self.assertEqual(result, "LINE")
+
+    
+
+    
+
+
+def gen_no_graph(*iso_codes, category: str, chart_type="") -> str:
+    print("chart type: ", chart_type)
+    return
+
+def gen_line_graph(*iso_codes, category: str, chart_type="LINE") -> str:
+    print("chart type: ", chart_type)
+    return
 
 if __name__ == "__main__":
     unittest.main()
