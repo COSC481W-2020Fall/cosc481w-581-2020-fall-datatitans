72c27d1d7041017d57b6160a74dfa11da6af5661
rhayes13 <rhayes13@emich.edu>
2020-10-25 23:39:16 -0400

    add bar test

--- datatitan_site/data/test_chart_type.py
+++ datatitan_site/data/test_chart_type.py
@@ -1,9 +1,16 @@
 import unittest
+# import forms.py
+# import generate_graphs.py
+
+# import sys
+# print (sys.path)
+# sys.path.insert(0, 'C:/Users/ryanp/git/cosc481w-581-2020-fall-datatitans/datatitan_site/data/scripts')
+# import generate_graphs
+
 
 
 class ChartTest(unittest.TestCase):
     
-    # Naming convention: start names of all test functions with the word "test"
     def setUp(self):
         # Arrange
         self.nocodes = ()
@@ -16,30 +23,38 @@ class ChartTest(unittest.TestCase):
         self.str1 = ()
     
     def test_no_chart(self):
-        result = gen_no_graph(*self.nocodes, category=str(""))
+        result = gen_no_chart(*self.nocodes, category=str(""))
         
         # Assert
-        self.assertEqual(result, None)
+        self.assertEqual(result, '')
 
     def test_line_chart(self):
         # No function to call in forms.py, other than init
-        result = gen_line_graph(*self.nocodes, category=str("LINE"))
-        result = "LINE"
+        result = gen_line_chart(*self.nocodes, category=str("LINE"))
         # Assert
         self.assertEqual(result, "LINE")
 
-    
+    def test_bar_chart(self):
+        # No function to call in forms.py, other than init
+        result = gen_bar_chart(*self.nocodes, category=str("BAR"))
+        # Assert
+        self.assertEqual(result, "BAR")
+
 
     
 
 
-def gen_no_graph(*iso_codes, category: str, chart_type="") -> str:
+def gen_no_chart(*iso_codes, category: str, chart_type="") -> str:
     print("chart type: ", chart_type)
-    return
+    return chart_type
 
-def gen_line_graph(*iso_codes, category: str, chart_type="LINE") -> str:
+def gen_line_chart(*iso_codes, category: str, chart_type="LINE") -> str:
     print("chart type: ", chart_type)
-    return
+    return chart_type
+
+def gen_bar_chart(*iso_codes, category: str, chart_type="BAR") -> str:
+    print("chart type: ", chart_type)
+    return chart_type
 
 if __name__ == "__main__":
     unittest.main()
