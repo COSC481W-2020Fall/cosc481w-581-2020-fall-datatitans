ee321a8991bdb8d2d8f9c6abaf6100c6a7375e17
Emmet Stanevich <emmet@stanevich.com>
2020-10-28 18:29:13 -0400

    Removed "run" command
    This command barely even works, specifically filing whenever it doesn't do exactly what "runserver" would do. As such, I'm removing it. If you want this command back, I can look into properly registering it as a command.

--- datatitan_site/manage.py
+++ datatitan_site/manage.py
@@ -15,15 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    if len(sys.argv) > 1 and sys.argv[1] == "run":
-        make_migrate = ['manage', 'makemigrations']
-        migrate = ['manage', 'migrate']
-        runserver = ['manage', 'runserver']
-        execute_from_command_line(make_migrate)
-        execute_from_command_line(migrate)
-        execute_from_command_line(runserver)
-    else:
-        execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
