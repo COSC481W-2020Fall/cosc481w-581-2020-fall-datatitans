e70a64047dd61ff2fb447aa54b296dfb4d16ba4a
Emmet Stanevich <emmet@stanevich.com>
2020-11-13 14:17:31 -0500

    Moved blog templates and views to blog app
    This will help keep the blog and the data resources separate from each other.

--- datatitan_site/blog/views.py
+++ datatitan_site/blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post, Comment
+from data.forms import CommentForm
 
 # Create your views here.
+
+
+def blog(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "blog/blog.html", {"posts": posts})
+
+
+def blog_detail(request, blog_id):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.username = request.username
+            comment.text = request.text
+            comment.created_date = timezone.now()
+            comment.blog_id = blog_id
+            comment.save()
+    blog_post = Post.objects.get(pk=blog_id)
+    comments = Comment.objects.get(blog_id=blog_id)
+
+    return render(
+        request,
+        "blog/blog_detail.html",
+        {"post": blog_post, "comments": comments, "form": CommentForm()},
+    )
--- datatitan_site/data/urls.py
+++ datatitan_site/data/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
+import blog.views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('blog/', views.blog, name='blog'),
-    path('blog_detail/<blog_id>', views.blog_detail, name='blog_detail'),
+    path('blog/', blog.views.blog, name='blog'),
+    path('blog_detail/<blog_id>', blog.views.blog_detail, name='blog_detail'),
 ]
