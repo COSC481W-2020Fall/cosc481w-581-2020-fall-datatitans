87a0ccc5e6dc77f5926d4d2638a1f83482aea37c
Emmet Stanevich <emmet@stanevich.com>
2020-11-14 17:36:56 -0500

    Began utilizing environment variables
    The secret key will no longer be included in version control, and will either be stored as an environment variable, or as a secret. I will also migrate the reference to the secrets into app.yaml's environment variables, in order to decouple the project name from the code, as well as to potentially allow for different secrets to be accessed depending on whether the site is being run locally or on the cloud.

--- datatitan_site/datatitan_site/settings.py
+++ datatitan_site/datatitan_site/settings.py
@@ -16,6 +16,9 @@ from google.cloud import secretmanager
 import google.auth
 import google.auth.transport.requests
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "(^i4r9k3ncsb7p1^z6o2(&n1-11uk_iy06qud6zb-@ho_^*$q5"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").casefold() != "false"
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "data-titans.uc.r.appspot.com"]
 
@@ -81,10 +84,13 @@ WSGI_APPLICATION = "datatitan_site.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 POSTGRES_USER = os.getenv("POSTGRES_USER", "DataTitans")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_PASSWORD_FILE = os.getenv(
     "POSTGRES_PASSWORD_FILE", BASE_DIR.parent / "cred" / "postgres_password.txt"
 )
 APP_ENV = os.getenv("APP_ENV")
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_OAUTH2_SECRET")
 
 if APP_ENV == "docker-compose":
     # Only attempt to access the postgres daemon if you think you're running in a docker container
@@ -105,9 +111,12 @@ elif APP_ENV == "google-app-engine":
     creds.refresh(auth_req)
     client = secretmanager.SecretManagerServiceClient()
     db_user = client.access_secret_version(
-        name="projects/984278497023/secrets/DataTitans-Postgres-Account/versions/latest"
+        name=os.getenv("POSTGRES_ACCOUNT_SECRET_ID")
     )
     account = json.loads(db_user.payload.data)
+    oauth_credentials = json.loads(client.access_secret_version(
+        name=os.getenv("OAUTH_CREDENTIALS_SECRET_ID")
+    ).payload.data)
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
@@ -119,6 +128,9 @@ elif APP_ENV == "google-app-engine":
             **account,
         }
     }
+    SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = oauth_credentials["web"]["client_id"]
+    SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = oauth_credentials["web"]["client_secret"]
+    SECRET_KEY = client.access_secret_version(os.getenv("DJANGO_SECRET_KEY_ID")).payload.data
 else:
     with Path(POSTGRES_PASSWORD_FILE).open("r") as file:
         DATABASES = {
