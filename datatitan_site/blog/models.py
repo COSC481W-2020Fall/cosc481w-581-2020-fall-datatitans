8975d6f2e8fb6be75e85925733fd4179331b6b1d
Emmet Stanevich <emmet@stanevich.com>
2020-11-14 18:57:01 -0500

    Changed post model to use a foreign key
    Blog posts will contain a reference to the user that posted them.

--- datatitan_site/blog/models.py
+++ datatitan_site/blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
