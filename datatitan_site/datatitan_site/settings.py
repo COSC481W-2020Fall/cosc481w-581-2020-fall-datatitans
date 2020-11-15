7ee5b87f9daea6dcfbf7e4a1f748293c14c80c3c
Emmet Stanevich <emmet@stanevich.com>
2020-11-14 19:45:57 -0500

    Added social auth pipeline arguments to settings
    I think this will allow us to associate additional login methods for the same account, but I will have to double-check.

--- datatitan_site/datatitan_site/settings.py
+++ datatitan_site/datatitan_site/settings.py
@@ -202,6 +202,18 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
 
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+]
+
 SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = "/data/"
 LOGOUT_REDIRECT_URL = "/data/"
