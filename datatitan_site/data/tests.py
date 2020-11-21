fff6dab5f9752e30b36bee176c9778de7d457c78
Emmet Stanevich <emmet@stanevich.com>
2020-11-21 11:46:30 -0500

    Began fixing tests
    Due to the overhaul in how data is being stored and presented, the current tests aren't necessarily going to fit.

--- datatitan_site/data/tests.py
+++ datatitan_site/data/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from data.models import CovidDataRaw, CovidDataClean, Country
+from data.models import Country
 from blog.models import Post, Comment
 from data.scripts.generate_graphs import gen_graph
 from django.utils import timezone
@@ -8,59 +8,62 @@ from data.scripts.database_handler import input_file_path, initialize_table
 import urllib.request
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+import json
 
 
 # Create your tests here.
 
 
 class DatabaseTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        """Initialize the test database, and read the input csv file into a pandas dataframe"""
-        cls.raw_data: pd.DataFrame = pd.read_csv(
-            "https://covid.ourworldindata.org/data/owid-covid-data.csv"
-        )
-        decimals = {
-            "stringency_index": 2,
-            "median_age": 1,
-            "extreme_poverty": 1,
-            "diabetes_prevalence": 2,
-            "life_expectancy": 2,
-        }
-        cls.raw_data = (
-            cls.raw_data.round(decimals=3)
-            .round(decimals=decimals)
-            .where(cls.raw_data.notnull(), None)
-        )
-        initialize_table()
+    fixtures = ["country_data.json"]
 
-    def test_upload(self) -> None:
-        """Verify that the number of entries in the database match the data pulled from the csv file"""
-        stored_raw_data = CovidDataRaw.objects.values("iso_code", "date")
-        self.assertEqual(stored_raw_data.count(), len(self.raw_data), f"Expected number of rows: {len(self.raw_data)}; Actual number of rows: {stored_raw_data.count()}")
+    # @classmethod
+    # def setUpTestData(cls):
+    #     """Initialize the test database, and read the input csv file into a pandas dataframe"""
+    #     cls.raw_data: pd.DataFrame = pd.read_csv(
+    #         "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+    #     )
+    #     decimals = {
+    #         "stringency_index": 2,
+    #         "median_age": 1,
+    #         "extreme_poverty": 1,
+    #         "diabetes_prevalence": 2,
+    #         "life_expectancy": 2,
+    #     }
+    #     cls.raw_data = (
+    #         cls.raw_data.round(decimals=3)
+    #         .round(decimals=decimals)
+    #         .where(cls.raw_data.notnull(), None)
+    #     )
+    #     initialize_table()
 
-    def test_materialized_views(self) -> None:
-        """Verify that the materialized views work for the test database"""
-        clean_data_count = len(self.raw_data.dropna(subset=["iso_code", "continent"]))
-        stored_clean_data_count = CovidDataClean.objects.count()
-        self.assertNotEqual(stored_clean_data_count, 0)
-        self.assertEqual(
-            stored_clean_data_count,
-            clean_data_count,
-            f"Expected number of rows: {clean_data_count}; Actual number of rows: {stored_clean_data_count}",
-        )
+    # def test_upload(self) -> None:
+    #     """Verify that the number of entries in the database match the data pulled from the csv file"""
+    #     stored_raw_data = CovidDataRaw.objects.values("iso_code", "date")
+    #     self.assertEqual(stored_raw_data.count(), len(self.raw_data), f"Expected number of rows: {len(self.raw_data)}; Actual number of rows: {stored_raw_data.count()}")
+    #
+    # def test_materialized_views(self) -> None:
+    #     """Verify that the materialized views work for the test database"""
+    #     clean_data_count = len(self.raw_data.dropna(subset=["iso_code", "continent"]))
+    #     stored_clean_data_count = CovidDataClean.objects.count()
+    #     self.assertNotEqual(stored_clean_data_count, 0)
+    #     self.assertEqual(
+    #         stored_clean_data_count,
+    #         clean_data_count,
+    #         f"Expected number of rows: {clean_data_count}; Actual number of rows: {stored_clean_data_count}",
+    #     )
 
     def test_graph(self) -> None:
         """Verify that the graph generator outputs a graph"""
         result = gen_graph("USA", category="deaths", chart_type="LINE")
-        self.assertIs(type(result), str, "Test failed: output is not a string.")
-        self.assertNotEqual(result, "", "Test failed: graph was not generated.")
+        self.assertIs(type(result), dict, "Test failed: output is not a dict.")
+        self.assertNotEqual(result, {}, "Test failed: graph was not generated.")
 
     def test_graph_without_codes(self) -> None:
         """Verify that the graph generator does not output a graph when provided with no countries"""
         result = gen_graph(*[], category="cases", chart_type="LINE")
-        self.assertIs(type(result), str, "Test failed: output is not a string.")
-        self.assertEqual(result, "", "Test failed: graph has been generated.")
+        self.assertIs(type(result), str, "Test failed: output is not a dict.")
+        self.assertEqual(result, {}, "Test failed: graph has been generated.")
 
 
 class BlogTestCase(TestCase):
@@ -85,18 +88,28 @@ class BlogTestCase(TestCase):
         for key, val in self.blog_post.items():
             self.assertEqual(val, self.test_post.__getattribute__(key))
 
-class CommentTestCase(TestCase):
 
-    def setup(self):
-        blog_post = Post.objects.create(author="Admin", title="Test Blog", text="This is a piece of blog text")
-        Comment.objects.create(username="Fred", text="I am Mr. Flintstone", created_date=timezone.now(),blog_id=blog_post.id )
+class CommentTestCase(TestCase):
+    def setUp(self):
+        blog_post = Post.objects.create(
+            author="Admin", title="Test Blog", text="This is a piece of blog text"
+        )
+        Comment.objects.create(
+            username="Fred",
+            text="I am Mr. Flintstone",
+            created_date=timezone.now(),
+            blog_id=blog_post.id,
+        )
 
     def test_comment_exists(self):
         comment = Comment.objects.get(username="Fred")
 
         self.assertEqual(comment.text, "I am Mr. Flintstone")
 
+
 class TableTestCase(StaticLiveServerTestCase):
+    fixtures = ["country_data.json"]
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -112,9 +125,7 @@ class TableTestCase(StaticLiveServerTestCase):
     def test_graph_title(self):
         """Checks to make sure the title of the graph matches the expected title."""
         self.selenium.get(self.live_server_url)
-        search_box = self.selenium.find_element_by_css_selector(
-            "input#country_search"
-        )
+        search_box = self.selenium.find_element_by_css_selector("input#country_search")
         country_list = self.selenium.find_element_by_css_selector("ul#id_iso_code")
         search_box.send_keys("united states")
         self.assertEqual(
@@ -128,9 +139,16 @@ class TableTestCase(StaticLiveServerTestCase):
             ),
             2,
         )
-        country_list.find_element_by_css_selector("input[type='checkbox'][value='USA']").click()
-        self.selenium.find_element_by_css_selector("#country_select_form>button[type='submit']").click()
-        self.assertRegex(self.selenium.find_elements_by_css_selector(
-            "g.mpld3-baseaxes > text.mpld3-text"
-        )[2].text, r"^(Total|New)\s(Cases|Deaths|Tests)\s((Per\s(Million|Thousand)\s)|)in\s")
-
+        country_list.find_element_by_css_selector(
+            "input[type='checkbox'][value='USA']"
+        ).click()
+        self.selenium.find_element_by_css_selector(
+            "#country_select_form>button[type='submit']"
+        ).click()
+        self.selenium.find_element_by_id("graph_data")
+        self.assertRegex(
+            self.selenium.find_elements_by_css_selector(
+                "g.mpld3-baseaxes > text.mpld3-text"
+            )[2].text,
+            r"^(Total|New)\s(Cases|Deaths|Tests)\s((Per\s(Million|Thousand)\s)|)in\s",
+        )
