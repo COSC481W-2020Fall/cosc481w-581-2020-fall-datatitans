ee49ea31497a907e6ec8c110093f76851e03f82e
Emmet Stanevich <emmet@stanevich.com>
2020-11-30 12:04:47 -0500

    Reformatted merged files via black
    This is mainly to make the code a bit more readable.

--- datatitan_site/data/machine_learning/TestDFforML.py
+++ datatitan_site/data/machine_learning/TestDFforML.py
@@ -1,4 +1,3 @@
-
 # Using a partial dataset from owid, create a dataframe with the necessary columns for the machine learning algorithm
 # Then clean them up by prefilling NaN and None fields; changing the Date column to Date/Time format
 # The output is written to a file in the data directory (data/input)
@@ -6,17 +5,18 @@
 import numpy as np
 import pandas as pd
 import datetime
+
 data_dir = "..\\input\\"
 
-df = pd.read_csv(data_dir + 'testdata.csv')
+df = pd.read_csv(data_dir + "testdata.csv")
 print(df.head())
-print((df['date'][0]))
-print(type(df['date'][0]))
+print((df["date"][0]))
+print(type(df["date"][0]))
 
 len(df)
 
 # Show all ISO_CODEs
-codes = df['iso_code'].unique()
+codes = df["iso_code"].unique()
 codes
 
 # create a list of column names, then strip extra spaces and convert to upper case
@@ -29,11 +29,11 @@ print(cols)
 # print(type(cols))
 
 # Convert date column to date/time type
-df['DATE'] = pd.to_datetime(df['DATE'])
+df["DATE"] = pd.to_datetime(df["DATE"])
 
-print(type(df['DATE'][0]))
+print(type(df["DATE"][0]))
 
-df = df.set_index('ISO_CODE')
+df = df.set_index("ISO_CODE")
 df.head()
 
 for c in cols:
@@ -41,15 +41,15 @@ for c in cols:
 
 df[250:260]
 
-c = 'USA'
+c = "USA"
 df.loc[c]
 
 # Fill in missing values using ffill; leading NaN values will NOT be replaced
 
 for c in codes:
-    df.loc[c] = df.loc[c].fillna(method='ffill')
+    df.loc[c] = df.loc[c].fillna(method="ffill")
 
 df[250:260]
 
 # Write df to a csv file
-df.to_csv(data_dir+'testout.csv',index=False)
+df.to_csv(data_dir + "testout.csv", index=False)
--- datatitan_site/data/machine_learning/test_ml.py
+++ datatitan_site/data/machine_learning/test_ml.py
@@ -22,67 +22,63 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.arima_model import ARIMA
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 # data is stored in the same directory
-dataUSA = pd.read_csv('US-MainData.csv')
+dataUSA = pd.read_csv("US-MainData.csv")
 
-#dataUSA.head()
+# dataUSA.head()
 
-print(dataUSA.loc[:4,['date','new_deaths']])
+print(dataUSA.loc[:4, ["date", "new_deaths"]])
 
 # Create an empty dataframe
-deathsUSA = pd.DataFrame(columns=['Date', 'New Deaths'])
+deathsUSA = pd.DataFrame(columns=["Date", "New Deaths"])
 
-#print(dataUSA.loc[58:80,['date','new_deaths']])
+# print(dataUSA.loc[58:80,['date','new_deaths']])
 
 # rows contain zero deaths until approx. row 60
-dates = dataUSA.loc[60:,'date']
-#print(dates.head())
+dates = dataUSA.loc[60:, "date"]
+# print(dates.head())
 
 # Convert to data format
 dates = list(pd.to_datetime(dates))
 
-#print(dates[:3])
+# print(dates[:3])
 
-deaths = dataUSA.loc[60:,'new_deaths']
-#print(deaths.head())
+deaths = dataUSA.loc[60:, "new_deaths"]
+# print(deaths.head())
 
 # Populate DataFrame
-deathsUSA['New Deaths'] = deaths
-deathsUSA['Date'] = dates
+deathsUSA["New Deaths"] = deaths
+deathsUSA["Date"] = dates
 
 # Convert first column to the index
-deathsUSA=deathsUSA.set_index('Date')
+deathsUSA = deathsUSA.set_index("Date")
 
 print(deathsUSA.tail())
 
 plt.figure(figsize=(10, 10))
 plt.plot(deathsUSA)
-plt.savefig('Cummulative daily deaths', bbox_inches='tight', transparent=False)
+plt.savefig("Cummulative daily deaths", bbox_inches="tight", transparent=False)
 
 # Set cutoff date at midway point of the date
-cutoff_date = '2020-07-08'
+cutoff_date = "2020-07-08"
 
 # Although train and test can be different lengths, subsequent calcs may need same length
 train = deathsUSA.loc[deathsUSA.index <= pd.to_datetime(cutoff_date)]
-## Remove the = from <= above if lengths of train, test don't have to be equal
+# Remove the = from <= above if lengths of train, test don't have to be equal
 test = deathsUSA.loc[deathsUSA.index >= pd.to_datetime(cutoff_date)]
 print(len(train), len(test))
 
-model = SARIMAX(train, order=(2,1,3)) # Arrived at these 3 params by trial and error
+model = SARIMAX(train, order=(2, 1, 3))  # Arrived at these 3 params by trial and error
 
 results = model.fit(disp=True)
 
-sarimax_prediction = results.predict(
-    start=cutoff_date, end='2020-11-15', dynamic=False)
-#print(len(sarimax_prediction))
+sarimax_prediction = results.predict(start=cutoff_date, end="2020-11-15", dynamic=False)
+# print(len(sarimax_prediction))
 
 plt.figure(figsize=(10, 5))
-l1, = plt.plot(deathsUSA, label='Observation')
-l2, = plt.plot(sarimax_prediction, label='Prediction')
+(l1,) = plt.plot(deathsUSA, label="Observation")
+(l2,) = plt.plot(sarimax_prediction, label="Prediction")
 plt.legend(handles=[l1, l2])
-plt.savefig('SARIMAX prediction', bbox_inches='tight', transparent=False)
-
-
-
+plt.savefig("SARIMAX prediction", bbox_inches="tight", transparent=False)
