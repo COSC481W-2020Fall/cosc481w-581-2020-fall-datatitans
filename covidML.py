ce82fe0685a8fea0f3b5c9c8ef398941c48bedb2
Brian Urbancic <burbanci@emich.edu>
2020-12-01 11:08:51 -0500

    Updated covidML.py with DataFrame script
    Pasted code from TestDFforML.py and edited

--- covidML.py
+++ covidML.py
@@ -8,6 +8,46 @@ import pandas as pd
 
 
 def main():
+    
+    ## Below added by BPU from TestDFforML.py script
+    import datetime
+    data_dir = "\\datatitan_site\\data\\input\\"
+
+    df = pd.read_csv(data_dir + 'owid-covid-data-project.csv')
+    # Show all ISO_CODEs
+    # codes = df['iso_code'].unique()
+    # codes
+
+    # print('Number of ISO Codes:',len(codes))
+
+    # create a list of column names, then strip extra spaces and convert to upper case
+    cols = list(df.columns)
+    cols = [x.upper().strip() for x in cols]
+    df.columns = cols
+    print(df.head(0))
+    
+    # Convert date column to date/time type
+    df['DATE'] = pd.to_datetime(df['DATE'])
+    # print(type(df['DATE'][0]))
+
+    df = df.set_index('ISO_CODE')
+    #df.head()
+
+    #for c in cols:
+        #print(c)
+
+    # Fill in missing values using ffill; leading NaN values will NOT be replaced
+
+    for alpha in range(len(codes) - 2):
+        print('Processing ', codes[alpha])
+        df.loc[codes[alpha]] = df.loc[codes[alpha]].fillna(method='ffill')
+
+    # Write df to a csv file
+    #df.to_csv(data_dir+'testout.csv',index=False)
+    
+    ## End BPU additions
+    
+    
     factors = ["GDP", "PopDen"] #not sure how this will work, but I would like these to contain the headers of the data set or some other way of referencing what we are using as factors
     degree = len(factors)
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
