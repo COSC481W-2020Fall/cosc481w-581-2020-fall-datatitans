8afa5cbc1a56b55d38329ff48f0c3da74c89c46b
Emmet Stanevich <emmet@stanevich.com>
2020-12-16 01:26:55 -0500

    Added data view class
    Honestly, this probably didn't need to be a class, but the main difference this view makes is the fact that it uses pandas to directly plot the graphs. The graphs are currently converted into html through mpld3, but I'm looking into utilizing Bokeh. There's just a couple snags I need to iron out.

--- datatitan_site/data/views.py
+++ datatitan_site/data/views.py
@@ -7,6 +7,11 @@ from django.views.decorators.http import require_GET
 from data.forms import ChartSelector
 from django.views.decorators.cache import cache_page
 from django.db.models import F
+from django.views import View
+import pandas as pd
+import numpy as np
+import os
+import mpld3
 
 
 @require_GET
@@ -64,7 +69,7 @@ def data(request):
                 *countries,
                 category=str.lower(data_category),
                 chart_type=chart_type,
-                metric=metric
+                metric=metric,
             ),
             "country_selector": form.as_p(),
             "fields": (field.replace("_", " ").title() for field in table_fields),
@@ -78,6 +83,100 @@ def data(request):
             "country_table": None,
         },
     )
+
+
+class CovidDataView(View):
+    template_name = "data/data.html"
+
+    country_names = (
+        pd.read_parquet(os.environ["INPUT_FILE"], columns=["location"])
+        .droplevel("date")
+        .drop_duplicates()
+    )
+
+    table_columns = (
+        "location",
+        "population",
+        "total_cases",
+        "total_deaths",
+        "total_tests",
+        "total_cases_per_million",
+        "total_deaths_per_million",
+        "total_tests_per_thousand",
+    )
+
+    def get(self, request, *args, **kwargs):
+        form = ChartSelector(request.GET)
+        if form.is_valid():
+            countries = form.cleaned_data["iso_code"].values_list("iso_code", flat=True)
+            data_category: str = form.cleaned_data["data_type"].lower()
+            chart_type = form.cleaned_data["chart_type"].lower()
+            metric = form.cleaned_data["metric"].lower()
+            category_name = f"""{"new" if chart_type.lower() == "bar" else "total"}_{data_category.lower()}"""
+            if metric == "normalized":
+                category_name += (
+                    f"""_per_{"thousand" if data_category == "tests" else "million"}"""
+                )
+            df: pd.DataFrame = pd.read_parquet(
+                os.environ["INPUT_FILE"],
+                columns=list(np.unique([*self.table_columns, category_name])),
+            ).loc[list(countries)]
+            country_stats: pd.DataFrame = (
+                df[list(self.table_columns)]
+                .groupby(level="iso_code", observed=True)
+                .last()
+                .copy()
+            )
+            df = df[[category_name]]
+            df = df.loc[df.index.dropna()]
+            title = (
+                category_name.replace("_", " ").title()
+                + " in "
+                + ", ".join(self.country_names.loc[list(countries)]["location"])
+            )
+            if chart_type == "bar":
+                df = (
+                    df.groupby(
+                        [
+                            pd.Grouper(level="iso_code"),
+                            pd.Grouper(level="date", freq="M"),
+                        ],
+                        observed=True,
+                    )
+                    .sum()
+                    .unstack(level="iso_code")
+                    .droplevel(axis=1, level=0)
+                )
+                graph_options = {"kind": "bar", "stacked": True, "xlabel": "Month"}
+            else:
+                df = df.unstack(level="iso_code").droplevel(axis=1, level=0)
+                graph_options = {"kind": "line"}
+            return render(
+                request,
+                self.template_name,
+                {
+                    "chart": mpld3.fig_to_html(
+                        df.plot(
+                            title=title,
+                            ylabel=data_category.capitalize(),
+                            **graph_options,
+                        ).figure
+                    ),
+                    "country_selector": form.as_p(),
+                    "country_table": country_stats.to_html(),
+                },
+            )
+        else:
+            return render(
+                request,
+                self.template_name,
+                {
+                    "country_selector": ChartSelector().as_p(),
+                },
+            )
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 def testing_map(request):
